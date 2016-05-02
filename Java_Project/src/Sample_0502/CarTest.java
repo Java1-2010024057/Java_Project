b8dded5621c82805f22bca030c91ecfec95562f7
Chanbyeol Jeong <mystar3918@naver.com>
2016-05-02 20:08:57 +0900

    0502

---
+++ Java_Project/src/Sample_0502/CarTest.java
@@ -0,0 +1,21 @@
+package Sample_0502;
+
+public class CarTest {
+	
+	//static Car c1= new Car("1", "2", 3);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(Car.numbers);
+		
+		//c1(main 밖에서 만들 때 static을 이용해야  main 에서 쓸 수 있다)
+		
+		Car c1 = new Car("Prius", "Light Gray", 200);
+		
+		System.out.println(Car.numbers);
+		
+		Car.Car_Numbers();
+	}
+	
+}
